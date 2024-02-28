4e11fa0731f4196d99a4b2748f83ce26fb9f3ac8
Umar <umar.317@gmail.com>
2024-02-28 18:46:14 +0500

    Update Bot

--- telegram/bot.js
+++ telegram/bot.js
@@ -1,17 +1,24 @@
 require("dotenv").config();
-const { Telegraf } = require("telegraf");
+const { Telegraf, Markup } = require("telegraf");
 const getAccount = require("../utils/createAccount");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply("Welcome"));
+bot.start((ctx) =>
+  ctx.reply(
+    `*🎯 Diablo Bot*\n\n*Trade Faster\\!*\n\n`
+    ,{parse_mode:'MarkdownV2',...Markup.inlineKeyboard([
+        Markup.button.callback("🪪 Create Wallet", "createWallet"),
+      ])}
+  )
+);
 
-bot.command("create", (ctx) => {
-    console.log('command received')
-    let {account,pk}= getAccount();
-    ctx.reply(`Account: ${account} \nPrivate Key: ${pk}`);
+bot.action("createWallet", (ctx) => {
+  const { address, pk } = getAccount();
+  ctx.reply(`✅ Successfully Created Wallet\n
+  \n⚠️ Save your private key\\. If you delete this message\\, we will not show you your private key again\\.\n\n💡 Private key: ${pk}\n\n📝 Address: \n${address}`, {
+    parse_mode: "MarkdownV2",
+  });
 });
-
-
 
 bot.launch();
--- utils/createAccount.js
+++ utils/createAccount.js
@@ -2,9 +2,9 @@ const {generatePrivateKey,privateKeyToAccount} = require('viem/accounts')
 
 function getAccount(){
     const pk = generatePrivateKey()
-    const account = privateKeyToAccount(pk).address
+    const address = privateKeyToAccount(pk).address
     //add logic to save account and pk to db
-    return {account:account,pk:pk}
+    return {address:address,pk:pk}
 }
 
 module.exports = getAccount
